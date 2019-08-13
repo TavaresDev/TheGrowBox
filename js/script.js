583285fe934b4e33d85c2267f7d65d4dee01e528
TavaresDev <vtavares.andre@gmail.com>
2019-08-13 13:41:04 -0400

    fix file name

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggle() {
+    let navBar = document.getElementById("topNavId")
+    if (navBar.className === "topNav") {
+        navBar.className += " responsive";
+    } else {
+        navBar.className = "topNav"
+    }
+
+}
