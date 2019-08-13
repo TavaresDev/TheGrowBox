3d54f7f77785ac4e2bcfd7aa6b91729cd35b4967
TavaresDev <vtavares.andre@gmail.com>
2019-08-13 13:31:53 -0400

    js folder

---
+++ js/navbar copy.js
@@ -0,0 +1,8 @@
+function toggle() {
+    let navBar = document.getElementById("topNavId")
+    if (navBar.className === "topNav") {
+        navBar.className += " responsive";
+    } else {
+        navBar.className = "topNav"
+    }
+}
